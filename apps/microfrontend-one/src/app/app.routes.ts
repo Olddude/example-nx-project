5624345014435783c7c960397729aea6f0984980
Konstantin Novikov <nov.konstantin@gmail.com>
2025-08-21 04:48:10 +0200

    sad

--- apps/microfrontend-one/src/app/app.routes.ts
+++ apps/microfrontend-one/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '/',
-    loadComponent: () => import('@example-nx-project/angular-components')
+    loadComponent: () => import('@example-nx-project/angular-shared')
       .then((m) => m.ExampleComponent)
   }
 ];
--- apps/shell/webpack.config.js
+++ apps/shell/webpack.config.js
@@ -1,9 +1,16 @@
 const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 
+// For production builds, we need to configure the remote URLs
+const remoteBaseUrl = process.env.PRODUCTION === 'true'
+  ? 'http://microfrontend-one:4201'
+  : 'http://localhost:4201';
+
 module.exports = withModuleFederation({
   ...config,
-  remotes: ['microfrontend-one'],
+  remotes: [
+    ['microfrontend-one', remoteBaseUrl]
+  ],
   shared: (libraryName, defaultConfig) => {
     if (libraryName === '@example-nx-project/angular-shared') {
       return {
