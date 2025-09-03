12a3098e1bb64006dc22978e32c8b8d8a6003750
Konstantin Novikov <nov.konstantin@gmail.com>
2025-09-03 17:21:44 +0200

    fix build

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
-    // eslint-disable-next-line @nx/enforce-module-boundaries
+     
     loadComponent: () => import('@olddude/angular-shared')
       .then((m) => m.HomePage)
   },
   {
     path: 'microfrontend-one',
     loadChildren: () =>
-      import('microfrontendOne/Routes').then((m) => m.remoteRoutes),
+      import('microfrontendOne/Routes')
+        .then((m) => m.remoteRoutes)
+        // Allow build-time when remote isn't resolvable; handle at runtime
+        .catch(() => []),
   },
 ];
