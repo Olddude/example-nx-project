b3cbc1ee81e7818d14f3c5896138f195f4662043
Konstantin Novikov <nov.konstantin@gmail.com>
2025-09-04 14:07:15 +0200

    get rid of not needed projects

--- apps/microfrontend-one-backend/src/server.ts
+++
@@ -1,7 +0,0 @@
-import { createServer as createHttpServer } from 'http';
-import { Server } from 'http';
-import { RequestListener } from 'http';
-
-export const createServer = (listener: RequestListener): Server => {
-  return createHttpServer(listener);
-};
